731a391c2534ff8ffd7ddc78156bea99a1f1cf00
Andrey Fadeyev <mass1veffect@gmail.com>
2013-03-21 12:26:54 +0000

    Rails app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyRotationProject::Application.config.secret_token = 'da5444e38c8efa292a59f4e0202e3d8fcc198ddae8e34db06aca17be2744816e77b1843c32ed60f14638c06da972102c16e966ef3228ff3cc78fd58232a0fb8b'
